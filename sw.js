29709dc088efc9c42bd91459d963869d6b7dcd1f
FranciscoRod6723 <63887397+FranciscoRod6723@users.noreply.github.com>
2024-08-20 14:47:05 -0600

    correcion sw

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const DYNAMIC_CHACE = 'dynamix_v1';
 const INMUTABLE_CHACE = 'inmutable_v1';
 
 const APP_SHELL = [
-    '/',
+    // '/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
